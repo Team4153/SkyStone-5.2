4dbe524b00e2f0e7144002ded6dcf63c14de6e96
haltosan <32181538+haltosan@users.noreply.github.com>
2019-11-22 16:57:19 -0800

    Update Tele.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStone/Tele.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
+//This is the worst tele op ever!
 @TeleOp(name="TELE OP")
 //@Disabled
 public class Tele extends OpMode
